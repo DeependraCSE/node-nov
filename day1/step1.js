dd6697754039c317b34a2ea3129e4c22d088a5e7
Node Training Files <139702010+nodefiles@users.noreply.github.com>
2024-11-23 22:47:57 +0530

    Create step1.js

---
+++ day1/step1.js
@@ -0,0 +1,16 @@
+let user = "Vijay";
+let company = "Intellipaat";
+let version = 10001;
+
+// object
+// module.exports.user = user;
+// module.exports.company = company;
+// module.exports.version = version;
+
+// module.exports = {
+//     user : user,
+//     company : company,
+//     version : version
+// }
+
+module.exports = { user, company, version }
