a34c1534634f850be1f3f64ab7316268afd0c3b6
Node Training Files <139702010+nodefiles@users.noreply.github.com>
2024-12-07 21:27:25 +0530

    Add files via upload

---
+++ day4/step3-config/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const config = require("./config.json");
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("welcome to your life")
+});
+// $env:PORT=5050
+// set PORT=5050
+// console.log(process.env.PORT);
+let port = process.env.PORT || config.port;
+
+app.listen(port,config.host, (error)=>{
+    if(error){
+        console.log("Error ", error);
+    }else{
+        console.log("web server is now live on localhost:",port)
+    }
+})
