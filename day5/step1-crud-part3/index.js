f47161f29a564bac66f687c6cad3a5b92bd830bc
Node Training Files <139702010+nodefiles@users.noreply.github.com>
2024-12-08 22:32:24 +0530

    Add files via upload

---
+++ day5/step1-crud-part3/index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.json());
+
+let localurl = "";
+let rawurl = "mongodb+srv://{{dbuser}}:{{pass}}@cluster0.{{dbstring}}.mongodb.net/{{dbname}}?retryWrites=true&w=majority&appName=Cluster0";
+let url = rawurl
+        .replace("{{dbuser}}",config.dbuser)
+        .replace("{{pass}}",config.dbpass)
+        .replace("{{dbstring}}",config.dbstring)
+        .replace("{{dbname}}",config.dbname)
+
+// connect to DB
+mongoose.connect(url)
+.then(()=> console.log("DB Connected"))
+.catch((error)=> console.log("Error ", error));
+
+// Model
+let Schema = mongoose.Schema;
+let ObjectId = Schema.ObjectId;
+
+let Friend = mongoose.model("Friend", new Schema({
+    id : ObjectId,
+    title : {
+        type: String
+    },
+    firstname : String,
+    lastname : String,
+    city : String,
+}))
+
+/* setTimeout(function(){
+    Friend.find().then(res => console.log(res)).catch(error => console.log("Error ", error))
+}, 2000); */
+
+// render the data
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/public/index.html");
+    // res.render("index.pug",{data : []})
+});
+// CREATE
+app.post("/data",(req, res) => {
+    console.log("req", req.body);
+    var friend = new Friend(req.body)
+    .save()
+    .then((dbres) => {
+        res.send({message : "data was added"})
+    })
+    .catch((error) => {
+        console.log("Error", error)
+    })
+})
+// READ
+app.get("/data", (req, res) => {
+    Friend
+    .find()
+    .then(dbres => res.send(dbres))
+    .catch(error => console.log("Error ", error))
+});
+// DELETE
+app.delete("/delete", (req, res) => {
+    // console.log("Delete request for ", req.body._id)
+    Friend.findByIdAndDelete({_id:req.body.id})
+    .then((dbres) => {res.send({message : "user deleted"})})
+    .catch((error) => {console.log("Error", error)})
+});
+
+app.listen(config.port,config.host, (error)=>{
+    error ? console.log("Error ", error) : console.log("Server is now live on ",config.host,":",config.port)
+})
